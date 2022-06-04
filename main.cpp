d1700cc31ee47a40e7d5405b78584c908dfcfaad
John S <johnschreiber84@gmail.com>
2022-06-04 17:07:18 +0200

    character_class

--- main.cpp
+++ main.cpp
@@ -1,124 +1,103 @@
 #include "raylib.h"
 #include "raymath.h"
 
-class Character 
+class Character
 {
 public:
     Vector2 getWorldPos() { return worldPos; }
+    void setScreenPos(int winWidth, int winHeight);
+    void tick(float deltaTime);
+
 private:
-    Texture2D texture;
-    Texture2D idle;
-    Texture2D run;
-    Vector2 screenPos;
-    Vector2 worldPos;
+    Texture2D texture{LoadTexture("clash_textures/player_idle.png")};
+    Texture2D idle{LoadTexture("clash_textures/player_idle.png")};
+    Texture2D run{LoadTexture("clash_textures/player_run.png")};
+    Vector2 screenPos{};
+    Vector2 worldPos{};
     // 1 : facing right, -1 : facing left
-    float rightLeft{1.f};
+    float rightLeft{1.0f};
     // animation variables
     float runningTime{};
     int frame{};
     const int maxFrames{6};
-    const float updateTime{1.f/12.f};
-
+    const float updateTime{1.f / 12.f};
+    const float speed{4.0f};
 };
+
+void Character::setScreenPos(int winWidth, int winHeight)
+{
+    screenPos = {
+        (float)winWidth / 2.0f - 4.0f * (0.5f * (float)texture.width / 6.0f),
+        (float)winHeight / 2.0f - 4.0f * (0.5f * (float)texture.height)};
+}
+
+void Character::tick(float deltaTime)
+{
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A))
+        direction.x -= 1.0f;
+    if (IsKeyDown(KEY_D))
+        direction.x += 1.0f;
+    if (IsKeyDown(KEY_W))
+        direction.y -= 1.0f;
+    if (IsKeyDown(KEY_S))
+        direction.y += 1.0f;
+
+    if (Vector2Length(direction) != 0.0f)
+    {
+        // set worldPos = worldPos + direction
+        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+        // check if character is moving left or right
+        direction.x < 0.0f ? rightLeft = -1.0f : rightLeft = 1.0f;
+        texture = run;
+    }
+    else
+    {
+        texture = idle;
+    }
+
+    // update animation frame by delta time
+    runningTime += deltaTime;
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.0f;
+        if (frame > maxFrames)
+            frame = 0;
+    }
+
+    // draw character
+    Rectangle source{frame * (float)texture.width / 6.0f, 0.0f, rightLeft * (float)texture.width / 6.0f, (float)texture.height};
+    Rectangle dest{screenPos.x, screenPos.y, 4.0f * (float)texture.width / 6.0f, 4.0f * (float)texture.height};
+    DrawTexturePro(texture, source, dest, Vector2{}, 0.0f, WHITE);
+}
 
 int main()
 {
-    // window dimensions and settings
-    const int windowWidth{ 384 };
-    const int windowHeight{ 384 };
-    SetTargetFPS( 60 );
-    const char* windowTitle { "Clash" };
-
+    const int windowWidth{384};
+    const int windowHeight{384};
+    const char *windowTitle{"Clash"};
     InitWindow(windowWidth, windowHeight, windowTitle);
-    
-    // textures
+
     Texture2D map = LoadTexture("clash_textures/WorldMap.png");
-    Texture2D hero = LoadTexture("clash_textures/player_idle.png");
-    Texture2D heroRun = LoadTexture("clash_textures/player_run.png");
-    Texture2D heroIdle = LoadTexture("clash_textures/player_idle.png");
+    Vector2 mapPos{0.0f, 0.0f};
 
-    // vectors
-    Vector2 mapPos { 0.0f, 0.0f };
-    Vector2 heroPos { 
-        (float)windowWidth / 2.0f - 4.0f * (0.5f * (float)hero.width / 6.0f),
-        (float)windowHeight / 2.0f - 4.0f *(0.5f * (float)hero.height)   
-    };
+    Character steve;
+    steve.setScreenPos(windowWidth, windowHeight);
 
-    float speed { 4.0f };
-
-    // value of 1 is facing right -1 facing left
-    float rightLeft { 1.0f };
-    // animation variables
-    float runningTime{};
-    int frame{};
-    const int maxFrame{6};
-    const float updateTime { 1.0f / 12.0f };
-
+    SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         // drawing start
         BeginDrawing();
         ClearBackground(WHITE);
-
-        ////
-        //   game logic starts
-        ////
-
-        // movement
-        Vector2 direction{};
-        if (IsKeyDown(KEY_A)) direction.x -= 1.0f ;
-        if (IsKeyDown(KEY_D)) direction.x += 1.0f ;
-        if (IsKeyDown(KEY_W)) direction.y -= 1.0f ;
-        if (IsKeyDown(KEY_S)) direction.y += 1.0f ;
-
-        if (Vector2Length(direction) != 0.0f)
-        {
-            // set mapPos = mapPos - direction
-            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
-            //check if character is moving left or right
-            direction.x < 0.0f ? rightLeft = -1.0f : rightLeft = 1.0f;
-            hero = heroRun;
-        }
-        else
-        {
-            hero = heroIdle;
-        }
-
-        // draw background
+  
+        mapPos = Vector2Scale(steve.getWorldPos(), -1.0f);
         DrawTextureEx(map, mapPos, 0.0f, 4.0f, WHITE);
 
-        // update animation frame by delyta time
-        runningTime += GetFrameTime();
-        if (runningTime >= updateTime)
-        {
-            frame++;
-            runningTime = 0.0f;
-            if (frame > maxFrame) frame = 0;
-        }
+        steve.tick(GetFrameTime());
 
-        // source sheet pos
-        Rectangle source {                      // position in the spritesheet 
-            frame * (float)hero.width / 6.0f,   // frame in row
-            0.0f,                               // first row idle animation
-            rightLeft * (float)hero.width / 6.0f, 
-            (float)hero.height 
-        };
-        // character location
-        Rectangle dest {                          // position on screen
-            heroPos.x, 
-            heroPos.y, 
-            4.0f * (float)hero.width /6.0f, 
-            4.0f * (float)hero.height
-        };
-        // draw character
-        DrawTexturePro(hero, source, dest, Vector2{}, 0.0f, WHITE);
-     
-        ////
-        //   game logic ends
-        ////
-
-        //drawing ends
+        // drawing ends
         EndDrawing();
     }
-
 }
