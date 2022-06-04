deb6c005217f21e87deb6216e6628ecaac7b7f4c
John S <johnschreiber84@gmail.com>
2022-06-04 15:54:25 +0200

    character_run_idle_animations

--- main.cpp
+++ main.cpp
@@ -26,11 +26,21 @@ int main()
 
     // value of 1 is facing right -1 facing left
     float rightLeft { 1.0f };
+    // animation variables
+    float runningTime{};
+    int frame{};
+    const int maxFrame{6};
+    const float updateTime { 1.0f / 12.0f };
+
     while (!WindowShouldClose())
     {
         // drawing start
         BeginDrawing();
         ClearBackground(WHITE);
+
+        ////
+        //   game logic starts
+        ////
 
         // movement
         Vector2 direction{};
@@ -39,26 +49,47 @@ int main()
         if (IsKeyDown(KEY_W)) direction.y -= 1.0f ;
         if (IsKeyDown(KEY_S)) direction.y += 1.0f ;
 
+        // set idle or run animation rectangle in spritesheet
+        Rectangle source{};
+        Rectangle sourceIdle { // position in the spritesheet 
+            frame * (float)hero.width / 6.0f, // frame in row
+            0.0f, // first row idle animation
+            rightLeft * (float)hero.width / 6.0f, 
+            (float)hero.height / 5.0f 
+        };
+        Rectangle sourceRun { // position in the spritesheet 
+            frame * (float)hero.width / 6.0f, // frame in row
+            hero.height / 5.0f, // second row running animation
+            rightLeft * (float)hero.width / 6.0f, 
+            (float)hero.height / 5.0f 
+        };
+
         if (Vector2Length(direction) != 0.0f)
         {
             // set mapPos = mapPos - direction
             mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
             //check if character is moving left or right
             direction.x < 0.0f ? rightLeft = -1.0f : rightLeft = 1.0f;
-
+            source = sourceRun;
+        }
+        else
+        {
+            source = sourceIdle;
         }
 
         // draw background
         DrawTextureEx(map, mapPos, 0.0f, 4.0f, WHITE);
 
-        // draw character
-        Rectangle source { // position in the spritesheet 
-            0.0f, // first sprite in row
-            (float)hero.height / 5.0f, // second row
-            rightLeft * (float)hero.width / 6.0f, 
-            (float)hero.height / 5.0f 
-        };
+        // update animation frame by delyta time
+        runningTime += GetFrameTime();
+        if (runningTime >= updateTime)
+        {
+            frame++;
+            runningTime = 0.0f;
+            if (frame > maxFrame) frame = 0;
+        }
 
+        // character location
         Rectangle dest { // position on screen
             heroPos.x, 
             heroPos.y, 
@@ -66,12 +97,9 @@ int main()
             4.0f * (float)hero.height / 5.0f 
         };
 
+        // draw character
         DrawTexturePro(hero, source, dest, Vector2{}, 0.0f, WHITE);
-
-        ////
-        //   game logic starts
-        ////
-        
+     
         ////
         //   game logic ends
         ////
