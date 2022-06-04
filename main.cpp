31920583c1aa71803f9b7393bcf6c0f3afcd84df
John S <johnschreiber84@gmail.com>
2022-06-04 16:34:27 +0200

    changed_sprites_added_character_class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include "raylib.h"
 #include "raymath.h"
 
+class Character 
+{
+public:
+    Vector2 getWorldPos() { return worldPos; }
+private:
+    Texture2D texture;
+    Texture2D idle;
+    Texture2D run;
+    Vector2 screenPos;
+    Vector2 worldPos;
+    // 1 : facing right, -1 : facing left
+    float rightLeft{1.f};
+    // animation variables
+    float runningTime{};
+    int frame{};
+    const int maxFrames{6};
+    const float updateTime{1.f/12.f};
+
+};
+
 int main()
 {
     // window dimensions and settings
@@ -13,13 +33,15 @@ int main()
     
     // textures
     Texture2D map = LoadTexture("clash_textures/WorldMap.png");
-    Texture2D hero = LoadTexture("clash_textures/player.png");
+    Texture2D hero = LoadTexture("clash_textures/player_idle.png");
+    Texture2D heroRun = LoadTexture("clash_textures/player_run.png");
+    Texture2D heroIdle = LoadTexture("clash_textures/player_idle.png");
 
     // vectors
     Vector2 mapPos { 0.0f, 0.0f };
     Vector2 heroPos { 
         (float)windowWidth / 2.0f - 4.0f * (0.5f * (float)hero.width / 6.0f),
-        (float)windowHeight / 2.0f - 4.0f *(0.5f * (float)hero.height / 5.0f)   
+        (float)windowHeight / 2.0f - 4.0f *(0.5f * (float)hero.height)   
     };
 
     float speed { 4.0f };
@@ -49,32 +71,17 @@ int main()
         if (IsKeyDown(KEY_W)) direction.y -= 1.0f ;
         if (IsKeyDown(KEY_S)) direction.y += 1.0f ;
 
-        // set idle or run animation rectangle in spritesheet
-        Rectangle source{};
-        Rectangle sourceIdle { // position in the spritesheet 
-            frame * (float)hero.width / 6.0f, // frame in row
-            0.0f, // first row idle animation
-            rightLeft * (float)hero.width / 6.0f, 
-            (float)hero.height / 5.0f 
-        };
-        Rectangle sourceRun { // position in the spritesheet 
-            frame * (float)hero.width / 6.0f, // frame in row
-            hero.height / 5.0f, // second row running animation
-            rightLeft * (float)hero.width / 6.0f, 
-            (float)hero.height / 5.0f 
-        };
-
         if (Vector2Length(direction) != 0.0f)
         {
             // set mapPos = mapPos - direction
             mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
             //check if character is moving left or right
             direction.x < 0.0f ? rightLeft = -1.0f : rightLeft = 1.0f;
-            source = sourceRun;
+            hero = heroRun;
         }
         else
         {
-            source = sourceIdle;
+            hero = heroIdle;
         }
 
         // draw background
@@ -89,14 +96,20 @@ int main()
             if (frame > maxFrame) frame = 0;
         }
 
+        // source sheet pos
+        Rectangle source {                      // position in the spritesheet 
+            frame * (float)hero.width / 6.0f,   // frame in row
+            0.0f,                               // first row idle animation
+            rightLeft * (float)hero.width / 6.0f, 
+            (float)hero.height 
+        };
         // character location
-        Rectangle dest { // position on screen
+        Rectangle dest {                          // position on screen
             heroPos.x, 
             heroPos.y, 
             4.0f * (float)hero.width /6.0f, 
-            4.0f * (float)hero.height / 5.0f 
+            4.0f * (float)hero.height
         };
-
         // draw character
         DrawTexturePro(hero, source, dest, Vector2{}, 0.0f, WHITE);
      
